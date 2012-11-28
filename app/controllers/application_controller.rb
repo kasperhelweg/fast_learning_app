ab3b7763762ea570e8f42a1c0a63e3d9173883cc
Kasper Helweg Jonassen <kasperhelweg@gmail.com>
2012-11-28 18:11:38 +0100

    ttt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for( resource )
     if current_user.role? :super_user
-      # Handle SuperUser
+      current_user
     else
       organization = current_user.organization
       organization_path( organization )
